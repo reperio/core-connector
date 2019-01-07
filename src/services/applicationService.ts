9ecaaaf3886819d7cede1c4da643832c5c1aefad
Andrew Robb <andrewrobb@sevenhillstechnology.com>
2019-01-07 18:12:40 -0500

    REP-389 Added a return type for the surveyUserSignup route

--- src/services/applicationService.ts
+++ src/services/applicationService.ts
@@ -1,6 +1,12 @@
 import {ReperioCoreConnector} from "./connector";
 import {SurveyPayload} from "../models/surveyPayload";
 
+interface SurveyResponse {
+    success: boolean;
+    errors: string[];
+
+}
+
 export class ApplicationService {
     constructor(public connector: ReperioCoreConnector) { }
 
@@ -12,7 +18,8 @@ export class ApplicationService {
         return await this.connector.axios.get(`/applications`, {baseURL: this.connector.config.baseURL});
     }
 
-    async surveyUserSignup(applicationId: string, surveyPayload: SurveyPayload) {
-        return await this.connector.axios.post(`/applications/${applicationId}/userSignup`, surveyPayload, {baseURL: this.connector.config.baseURL});
+    async surveyUserSignup(applicationId: string, surveyPayload: SurveyPayload) : Promise<SurveyResponse> {
+        const response = await this.connector.axios.post(`/applications/${applicationId}/userSignup`, surveyPayload, {baseURL: this.connector.config.baseURL});
+        return response.data;
     }
 }
